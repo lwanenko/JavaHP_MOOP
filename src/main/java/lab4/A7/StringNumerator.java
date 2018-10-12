540b4506dbad1db3fd02b58cebcb726a3fd7031b
Slawik Ivanenko <good.worker.1997@gmail.com>
2018-10-12 23:20:48 +0300

    lab4 A7 started

---
+++ src/main/java/lab4/A7/StringNumerator.java
@@ -0,0 +1,20 @@
+package lab4.A7;
+
+public class StringNumerator {
+
+    private String inputText;
+
+    public StringNumerator(String inputText) {
+        this.inputText = inputText;
+    }
+
+    public String parse(){
+        return "";
+    }
+
+    public void consolePrint(){
+        System.out.println(inputText);
+        System.out.println(parse());
+    }
+
+}
--- src/main/java/lab4/Runner.java
+++ src/main/java/lab4/Runner.java
@@ -1,5 +1,48 @@
 package lab4;
 
+import lab4.A7.StringNumerator;
+
 public class Runner {
 
+    public static void main(String[ ] args) {
+        System.out.println("MOOP Lab 4 Viacheslav Ivamemko BI-1 Var 2");
+        partA7();
+        partB7();
+        partC7();
+
+        partA8();
+        partB8();
+        partC8();
+    }
+
+    public static void partA7(){
+        System.out.println("A7");
+        StringNumerator numerator = new StringNumerator("ABCDEFG");
+        numerator.consolePrint();
+    }
+
+    public static void partA8(){
+        System.out.println("A8:");
+
+    }
+
+    public static void partB7(){
+        System.out.println("B7");
+
+    }
+
+    public static void partB8(){
+        System.out.println("B8:");
+
+    }
+
+    public static void partC7(){
+        System.out.println("C7");
+
+    }
+
+    public static void partC8(){
+        System.out.println("C8:");
+
+    }
 }
